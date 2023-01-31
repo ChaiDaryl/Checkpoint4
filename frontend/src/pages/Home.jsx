947d6b266d40a651c8468d3506364eb4e0918d83
ChaiDaryl <chaigne.d01@gmail.com>
2023-01-31 15:30:14 +0100

    init

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="d-flex h-100">
+    <div className="d-flex h-100 ">
       <h1 className="m-auto">Wild Library</h1>
     </div>
   );
